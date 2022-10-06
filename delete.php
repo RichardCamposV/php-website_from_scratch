2b5a80431df3bff88efb1c881fbe246649092b23
Ricardo Campos <ricard_halo@live.com.mx>
2022-10-06 16:49:26 -0500

    Login & logout

--- delete.php
+++ delete.php
@@ -1,6 +1,11 @@
 <?php
   require "database.php";
 
+  if (!isset($_SESSION["user"])) {
+    header("Location: login.php");
+    return;
+  }
+
   $id = $_GET["id"];
 
   $statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
